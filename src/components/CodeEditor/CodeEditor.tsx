058e912472777eb46305caf8eb7b765a1a48969b
yabandzhiev <yabandzhiev@abv.bg>
2022-06-02 13:59:50 +0300

    add sass

--- src/components/CodeEditor/CodeEditor.tsx
+++ src/components/CodeEditor/CodeEditor.tsx
@@ -3,6 +3,8 @@ import MonacoEditor, { EditorDidMount, monaco } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
 
+import "./CodeEditor.scss";
+
 interface CodeEditorProps {
   initialValue: string;
   onChange(value: string): void;
@@ -35,8 +37,13 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     editorRef.current.setValue(formatted);
   };
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
