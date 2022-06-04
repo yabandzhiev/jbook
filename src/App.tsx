55947a783181d07eae6889252fbbf9205cde551f
yabandzhiev <yabandzhiev@abv.bg>
2022-06-05 01:19:05 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,16 @@
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useState, useRef } from "react";
-
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
+import { useState } from "react";
 
 import CodeEditor from "./components/CodeEditor/CodeEditor";
 import Preview from "./components/Preview/Preview";
+import bundle from "./bundler";
 
 function App() {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
-  const ref = useRef<any>();
-
-  useEffect(() => {
-    startService();
-  }, []);
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
 
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
