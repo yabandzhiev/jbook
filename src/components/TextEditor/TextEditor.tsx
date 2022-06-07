a25246235ffb30caca2adb4d40a6d53903ded64c
yabandzhiev <yabandzhiev@abv.bg>
2022-06-07 12:48:09 +0300

    Update TextEditor.tsx

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,7 +1,37 @@
 import MDEditor from "@uiw/react-md-editor";
+import { useEffect, useState, useRef } from "react";
+import "./TextEditor.scss";
 
 const TextEditor: React.FC = () => {
-  return <div></div>;
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (ref.current && event.target && ref.current.contains(event.target as Node)) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+  return (
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
+    </div>
+  );
 };
 
 export default TextEditor;
