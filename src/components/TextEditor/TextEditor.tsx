033a2f184af978a8b7b9a50acd531bc79939bb5e
yabandzhiev <yabandzhiev@abv.bg>
2022-06-07 13:21:08 +0300

    Update TextEditor.tsx

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -5,6 +5,7 @@ import "./TextEditor.scss";
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -23,13 +24,13 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div className="text-editor" ref={ref}>
-        <MDEditor />
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
       </div>
     );
   }
   return (
     <div className="text-editor" onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={"# Header"} />
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
