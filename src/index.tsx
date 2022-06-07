8fb4b56a183dd305d88c423e82700647099826b1
yabandzhiev <yabandzhiev@abv.bg>
2022-06-07 12:48:16 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import TextEditor from "./components/TextEditor/TextEditor";
 
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <TextEditor />
   </React.StrictMode>,
   document.getElementById("root")
 );
