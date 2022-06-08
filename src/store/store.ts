1bffcc8648261a355bc5c611a5a664ddb4e2a479
yabandzhiev <yabandzhiev@abv.bg>
2022-06-08 18:41:35 +0300

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,37 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { ActionType } from "./action-types";
 
 export const store = createStore(
   reducers,
   {},
   applyMiddleware(thunk)
 );
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+});
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "text",
+  },
+});
+
+const id = store.getState().cells?.order[0];
+console.log(store.getState());
+
+store.dispatch({
+  type: ActionType.MOVE_CELL,
+  //@ts-ignore
+  payload: { id, direction: "down" },
+});
+
+console.log(store.getState());
