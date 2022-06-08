5d81e859a626d1a4dddf847a58a254b3153dc95f
yabandzhiev <yabandzhiev@abv.bg>
2022-06-08 15:31:24 +0300

    add provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import TextEditor from "./components/TextEditor/TextEditor";
 
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <TextEditor />
+    <Provider store={store}>
+      {/* <App /> */}
+
+      <TextEditor />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
