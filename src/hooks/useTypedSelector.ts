cef3f1200504e92faa50dc883823a348d0ccf4a9
yabandzhiev <yabandzhiev@abv.bg>
2022-06-09 10:28:46 +0300

    create hook

---
+++ src/hooks/useTypedSelector.ts
@@ -0,0 +1,8 @@
+import {
+  useSelector,
+  TypedUseSelectorHook,
+} from "react-redux";
+import { RootState } from "../store";
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> =
+  useSelector;
