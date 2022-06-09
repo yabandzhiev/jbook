489c359e723744917a509925d7b392f2bda81bfc
yabandzhiev <yabandzhiev@abv.bg>
2022-06-09 18:44:43 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import CodeCell from "./components/CodeCell/CodeCell";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
 import TextEditor from "./components/TextEditor/TextEditor";
 
-import "bulmaswatch/superhero/bulmaswatch.min.css";
 import CellList from "./components/CellList/CellList";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <App /> */}
+      {/* <CodeCell /> */}
 
       {/* <TextEditor /> */}
       <CellList />
