eb8d07c29a840d25b0147cb552d44a6fa3d2731f
yabandzhiev <yabandzhiev@abv.bg>
2022-06-11 12:15:39 +0300

    fill in logic for the reducer

--- src/store/reducers/bundlesReducer.ts
+++ src/store/reducers/bundlesReducer.ts
@@ -16,9 +16,19 @@ const reducer = produce(
   (state: BundlesState = initialState, action: Action): BundlesState => {
     switch (action.type) {
       case ActionType.BUNDLE_START:
+        state[action.payload.cellId] = {
+          loading: true,
+          code: "",
+          err: "",
+        };
         return state;
 
-      case ActionType.BUNDLE_START:
+      case ActionType.BUNDLE_COMPLETE:
+        state[action.payload.cellId] = {
+          loading: false,
+          code: action.payload.bundle.code,
+          err: action.payload.bundle.err,
+        };
         return state;
 
       default:
