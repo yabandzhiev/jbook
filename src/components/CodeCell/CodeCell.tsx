d1dd55866f0e8fb2bd613c712eab74ceb7aee360
yabandzhiev <yabandzhiev@abv.bg>
2022-06-13 02:04:03 +0300

    add loading bar

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -7,6 +7,8 @@ import { Cell } from "../../store";
 import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 
+import "./CodeCell.scss";
+
 interface CodeCellProps {
   cell: Cell;
 }
@@ -48,7 +50,15 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           />
         </Resizable>
 
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {!bundle || bundle.loading ? (
+          <div className="progress-cover">
+            <progress className="progress is-small is-primary" max="100">
+              Loading
+            </progress>
+          </div>
+        ) : (
+          <Preview code={bundle.code} err={bundle.err} />
+        )}
       </div>
     </Resizable>
   );
