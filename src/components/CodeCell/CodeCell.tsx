74e6b920e5dae55a3957188283a77fea6abbcfa5
yabandzhiev <yabandzhiev@abv.bg>
2022-06-13 01:56:41 +0300

    create bundle asap on render

--- src/components/CodeCell/CodeCell.tsx
+++ src/components/CodeCell/CodeCell.tsx
@@ -16,6 +16,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 750);
@@ -23,6 +28,8 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.id, cell.content, createBundle]);
 
   return (
